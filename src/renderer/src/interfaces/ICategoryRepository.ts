e6023e61dcec8e060a53119691d8669d922551b3
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-11-28 23:48:03 +0800

    update category repo

--- src/renderer/src/interfaces/ICategoryRepository.ts
+++ src/renderer/src/interfaces/ICategoryRepository.ts
@@ -3,6 +3,12 @@ import { CategoryType } from '@renderer/utils/types'
 export interface ICategoryRepository {
   getAll(): CategoryType[]
   get(id: number): CategoryType
-  create(name: string): void
-  update({ id, name }: { id: number; name: string }): void
+  create(name: string): {
+    success: boolean
+    error: string
+  }
+  update({ id, name }: { id: number; name: string }): {
+    success: boolean
+    error: string
+  }
 }
--- src/renderer/src/repository/CategoryRepository.ts
+++ src/renderer/src/repository/CategoryRepository.ts
@@ -30,14 +30,124 @@ export class CategoryRepository implements ICategoryRepository {
     return category
   }
 
-  create(name: string): void {
-    const stmt = this._database.prepare('INSERT INTO categories (name) VALUES ( ?)')
-    stmt.run(name)
+  create(name: string): { success: boolean; error: string } {
+    const normalizeName = name?.trim()
+
+    // const found = this._database
+    //   .prepare('SELECT id, name FROM categories WHERE name LIKE ?')
+    //   .get(`${normalizeName}%`)
+
+    // console.log('found', found)
+    // if (found && found.name.toLowerCase() === normalizeName.toLowerCase()) {
+    //   return {
+    //     success: false,
+    //     error: 'Category already existed'
+    //   }
+    // }
+    try {
+      const result = this._database
+        .prepare('INSERT INTO categories (name) VALUES ( ?) RETURNING *')
+        .get(normalizeName)
+
+      if (result) {
+        return {
+          success: true,
+          error: ''
+        }
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`Error: ${error.message}`)
+        console.log('error name', error.name)
+        // console.log('error', error.code)
+        if (error.name === 'SqliteError') {
+          return {
+            success: false,
+            error: 'Constraint violation: Data already exists or is invalid.'
+          }
+        }
+        // if (error.code === 'SQLITE_CONSTRAINT') {
+        //   console.error('Constraint violation: Data already exists or is invalid.')
+        //   return {
+        //     success: false,
+        //     error: 'Constraint violation: Data already exists or is invalid.'
+        //   }
+        // }
+        return {
+          success: false,
+          error: error.message
+        }
+      }
+      return {
+        success: false,
+        error: `${error}`
+      }
+    }
+
+    return {
+      success: false,
+      error: "Something wen't wrong"
+    }
   }
 
-  update({ id, name }: { id: number; name: string }): void {
+  update({ id, name }: { id: number; name: string }): { success: boolean; error: string } {
     console.log({ id, name })
-    const stmt = this._database.prepare('UPDATE categories SET name = ? WHERE id = ?')
-    stmt.run(name, id)
+
+    const normalizeName = name?.trim()
+
+    const found = this._database.prepare('SELECT id FROM categories WHERE name = ?').get(name)
+
+    console.log('found', found)
+    if (found && found.id !== id) {
+      return {
+        success: false,
+        error: 'Category already existed'
+      }
+    }
+
+    try {
+      const stmt = this._database.prepare('UPDATE categories SET name = ? WHERE id = ? RETURNING *')
+      const updatedRow = stmt.get(normalizeName, id)
+      console.log('updatedRow', updatedRow)
+      console.log(`Rows changed: ${updatedRow.changes}`)
+
+      if (updatedRow?.id) {
+        return {
+          success: true,
+          error: ''
+        }
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`Error: ${error.message}`)
+        console.log('error name', error.name)
+        // console.log('error', error.code)
+        if (error.name === 'SqliteError') {
+          return {
+            success: false,
+            error: 'Constraint violation: Data already exists or is invalid.'
+          }
+        }
+        // if (error.code === 'SQLITE_CONSTRAINT') {
+        //   console.error('Constraint violation: Data already exists or is invalid.')
+        //   return {
+        //     success: false,
+        //     error: 'Constraint violation: Data already exists or is invalid.'
+        //   }
+        // }
+        return {
+          success: false,
+          error: error.message
+        }
+      }
+      return {
+        success: false,
+        error: `${error}`
+      }
+    }
+    return {
+      success: false,
+      error: "something wen't wrong"
+    }
   }
 }
