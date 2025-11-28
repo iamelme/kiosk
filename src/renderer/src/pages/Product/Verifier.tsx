f888ca3bb9b752c8c7922b3a461928895d498a27
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-11-28 23:47:16 +0800

    add product verifier page

---
+++ src/renderer/src/pages/Product/Verifier.tsx
@@ -0,0 +1,9 @@
+import Input from '@renderer/components/ui/Input'
+
+export default function Verifier(): React.JSX.Element {
+  return (
+    <>
+      <Input placeholder="Search" />
+    </>
+  )
+}
