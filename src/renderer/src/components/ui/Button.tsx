e832b1688001914f630352c89a6ca5c485349ff9
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-12-02 12:55:56 +0800

    button add sizes prop

--- src/renderer/src/components/ui/Button.tsx
+++ src/renderer/src/components/ui/Button.tsx
@@ -2,14 +2,22 @@ import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const styles = {
-  default: 'bg-indigo-600 rounded-sm text-white',
-  danger: 'bg-red-600 text-white'
+  variants: {
+    default: 'bg-indigo-600 text-white',
+    danger: 'bg-red-600 text-white',
+    outline: 'border border-slate-200'
+  },
+  sizes: {
+    default: 'py-2 px-4',
+    icon: 'p-2'
+  }
 }
 
 type Styles = typeof styles
 
 type ButtonProps = {
-  variant?: keyof Styles
+  variant?: keyof Styles['variants']
+  size?: keyof Styles['sizes']
   className?: string
   children: ReactNode
 } & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
@@ -17,11 +25,18 @@ type ButtonProps = {
 export default function Button({
   className,
   variant = 'default',
+  size = 'default',
   children,
   ...props
 }: ButtonProps): React.JSX.Element {
   return (
-    <button {...props} className={twMerge(`${styles[variant]} py-1 px-4 `, className)}>
+    <button
+      {...props}
+      className={twMerge(
+        `inline-flex items-center ${styles.variants[variant]} ${styles.sizes[size]} rounded-sm  cursor-pointer`,
+        className
+      )}
+    >
       {children}
     </button>
   )
