e028515c1f8e3d4ada3ff99257ae0ddf4e3bbe64
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-12-04 18:41:20 +0800

    add virtual table for products

--- src/renderer/src/database/db.ts
+++ src/renderer/src/database/db.ts
@@ -52,6 +52,34 @@ export class AppDatabase {
             INSERT INTO counts (products, categories) 
             SELECT 0, 0
             WHERE NOT EXISTS (SELECT 1 FROM counts);
+            
+            CREATE VIRTUAL TABLE IF NOT EXISTS products_fts USING fts5(product_id, name, sku, code);
+
+            CREATE TRIGGER IF NOT EXISTS products_after_insert 
+            AFTER INSERT ON products
+            BEGIN
+
+              INSERT INTO products_fts (product_id, name, sku, code) 
+              VALUES(NEW.id, NEW.name, NEW.sku, NEW.code);
+            END;
+
+            CREATE TRIGGER IF NOT EXISTS products_after_update 
+            AFTER UPDATE ON products
+            BEGIN
+
+              UPDATE products_fts 
+              SET name = NEW.name, sku = NEW.sku, code = NEW.code
+              WHERE product_id = OLD.id;
+            END;
+
+            CREATE TRIGGER IF NOT EXISTS products_after_delete
+            AFTER DELETE ON products
+            BEGIN
+
+              DELETE FROM products_fts
+              WHERE product_id = OLD.id;
+            END;
+
             `
     )
 
