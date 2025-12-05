a8932181b98a74ae0799deefdd7efaa7274c9d1e
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-12-05 19:17:03 +0800

    add headers

--- src/renderer/src/components/Items.tsx
+++ src/renderer/src/components/Items.tsx
@@ -1,14 +1,31 @@
 import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type ItemsProps<T> = {
   items: T[]
+  headers: { label: string; className?: string }[]
   renderItems: (item: T) => ReactNode
 }
 
-export default function Items<T>({ items, renderItems }: ItemsProps<T>): React.JSX.Element {
+export default function Items<T>({
+  headers,
+  items,
+  renderItems
+}: ItemsProps<T>): React.JSX.Element {
   return (
-    <table className="w-full">
-      <tbody>{items?.map((item) => renderItems(item))}</tbody>
-    </table>
+    <div className="my-6 w-full overflow-y-auto">
+      <table className="w-full">
+        <thead>
+          <tr>
+            {headers?.map((header, idx) => (
+              <th key={idx} className={twMerge(`font-medium text-left ${header.className}`)}>
+                {header.label}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{items?.map((item) => renderItems(item))}</tbody>
+      </table>
+    </div>
   )
 }
