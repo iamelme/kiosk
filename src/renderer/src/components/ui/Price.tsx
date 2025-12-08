6ffbebc938c2c0995f61520e2facae26dfe792b8
Arcelie Ampo-on <iam.arcelie@Arcelies-MacBook-Pro.local>
2025-12-08 21:50:22 +0800

    add Price component

---
+++ src/renderer/src/components/ui/Price.tsx
@@ -0,0 +1,20 @@
+import useBoundStore from '@renderer/stores/boundStore'
+import { ReactNode } from 'react'
+import { getCurrency } from 'locale-currency'
+
+export default function Price({ value }: { value: number }): ReactNode {
+  const locale = useBoundStore((state) => state.locale)
+
+  if (isNaN(value)) {
+    return null
+  }
+
+  return (
+    <>
+      {new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: getCurrency(locale) as string
+      }).format(value)}
+    </>
+  )
+}
