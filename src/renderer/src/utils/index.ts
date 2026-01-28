387c0a805fc34766e1fdf57e4ebec6576019da67
iamelme <elme.delossantos@gmail.com>
2026-01-28 22:54:43 +0800

    append functions to add days and csv download

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -14,3 +14,37 @@ export const saleStatuses = [
   { label: 'Complete', value: 'complete' },
   { label: 'Void', value: 'void' }
 ]
+
+export function addDays(date: Date, days: number): string {
+  if (!date) {
+    return ''
+  }
+  const newDate = new Date(date)
+
+  newDate.setDate(date.getDate() + days)
+  return newDate.toISOString()
+}
+
+export function csvDownload<T>({
+  header,
+  data,
+  title
+}: {
+  header: Array<string>
+  data: T
+  title: string
+}): void {
+  const content = Array.isArray(data)
+    ? data?.map((d) => Object.values(d).join(', ')).join('\n')
+    : ''
+  const csvString = `${header}\n` + content
+  const uri = encodeURI('data:text/csv;charset=utf-8,' + csvString)
+
+  const link = document.createElement('a')
+  link.setAttribute('href', uri)
+  link.setAttribute('download', `${title}.csv`)
+  document.body.appendChild(link)
+
+  link.click()
+  document.body.removeChild(link)
+}
