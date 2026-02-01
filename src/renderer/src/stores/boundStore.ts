3ca55f943899aa398bdb838bb1bdeb8bd1e1f323
iamelme <elme.delossantos@gmail.com>
2026-02-01 11:32:09 +0800

    refactor: settings

--- src/renderer/src/stores/boundStore.ts
+++ src/renderer/src/stores/boundStore.ts
@@ -3,13 +3,43 @@ import { persist, createJSONStorage } from 'zustand/middleware'
 import { AppSlice, createAppSlice } from './slices/appSlice'
 import { createUserSlice, UserSlice } from './slices/userSlice'
 
-export const useBoundStore = create<AppSlice & UserSlice>((...a) => ({
-  ...createAppSlice(...a),
-  ...persist(createUserSlice, {
-    name: 'auth',
-    partialize: (state) => ({ user: state.user }),
-    storage: createJSONStorage(() => localStorage)
-  })(...a)
-}))
+export const useBoundStore = create<AppSlice & UserSlice>()(
+  persist(
+    (...a) => ({
+      ...createAppSlice(...a),
+      ...createUserSlice(...a)
+    }),
+    {
+      name: 'app',
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        user: state.user,
+        settings: {
+          ...state.settings,
+          general: {
+            ...state.settings.general,
+            logo: state.settings.general.logo
+          }
+        }
+      })
+    }
+  )
+  // ...createAppSlice(...a),
+  // // ...persist(createAppSlice, {
+  // //   name: 'logo',
+  // //   partialize: (state) => ({
+  // //     settings: {
+  // //       ...state.settings,
+  // //       logo: state.settings.general.logo
+  // //     }
+  // //   }),
+  // //   storage: createJSONStorage(() => localStorage)
+  // // })(...a),
+  // ...persist(createUserSlice, {
+  //   name: 'auth',
+  //   partialize: (state) => ({ user: state.user }),
+  //   storage: createJSONStorage(() => localStorage)
+  // })(...a)
+)
 
 export default useBoundStore
--- src/renderer/src/stores/slices/appSlice.ts
+++ src/renderer/src/stores/slices/appSlice.ts
@@ -1,11 +1,42 @@
 import { StateCreator } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
 export interface AppSlice {
-  locale: string
+  settings: {
+    general: {
+      logo: string
+      locale: string
+    }
+    cart: {
+      discountRate: number
+      discountType: string
+      tax: number
+    }
+  }
   updateLocale: (locale: string) => void
+  updateLogo: (logo: string) => void
 }
 
-export const createAppSlice: StateCreator<AppSlice, [], [], AppSlice> = (set) => ({
-  locale: 'en-US',
-  updateLocale: (locale: string) => set(() => ({ locale: locale }))
-})
+export const createAppSlice: StateCreator<AppSlice, [], [['zustand/immer', never]], AppSlice> =
+  immer((set) => ({
+    settings: {
+      general: {
+        logo: '',
+        locale: 'en-PH'
+      },
+      cart: {
+        discountRate: 0,
+        discountType: 'percentage',
+        tax: 12
+      }
+    },
+    // updateLocale: (locale: string) => set(() => ({ locale }))
+    updateLocale: (locale: string) =>
+      set((state) => {
+        state.settings.general.locale = locale
+      }),
+    updateLogo: (logo: string) =>
+      set((state) => {
+        state.settings.general.logo = logo
+      })
+  }))
