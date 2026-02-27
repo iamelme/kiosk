3ac1fb1c88ab8b8b36a99d7656f777d00996dbe0
iamelme <elme.delossantos@gmail.com>
2026-02-27 11:57:21 +0800

    chore: update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from 'path';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom'
   },
   esbuild: {
     drop: ["console", "debugger"],
+  },
+  resolve: {
+    alias: {
+      '@renderer': resolve(__dirname, './src/renderer/src'),
+    }
   },
 })
