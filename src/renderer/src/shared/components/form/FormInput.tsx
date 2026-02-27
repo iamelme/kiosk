068aa1b48318e50beed5eef9aa71c7c0c9b9a2f3
iamelme <elme.delossantos@gmail.com>
2026-02-27 14:54:22 +0800

    refactor: update layout if there's no label

--- src/renderer/src/shared/components/form/FormInput.tsx
+++ src/renderer/src/shared/components/form/FormInput.tsx
@@ -36,18 +36,16 @@ export default function FormInput({
   }, [name, setValue, fieldValue])
 
   return (
-    <div className="mb-4">
-      <div className="md:grid grid-cols-7 gap-x-5">
+    <div className={`${label ? "md:grid grid-cols-7 gap-x-5" : ""} mb-4`}>
+      {label && (
         <div className="col-span-3">
 
-          {label && (
-            <label
-              className={`flex mb-1 font-medium text-md ${errors[name] ? 'text-red-500' : ''}`}
-              htmlFor={name}
-            >
-              {label} {props.required && <span className='text-red-500'>*</span>}
-            </label>
-          )}
+          <label
+            className={`flex mb-1 font-medium text-md ${errors[name] ? 'text-red-500' : ''}`}
+            htmlFor={name}
+          >
+            {label} {props.required && <span className='text-red-500'>*</span>}
+          </label>
           {
             props?.helperText &&
             <p className="text-xs text-slate-500">
@@ -55,38 +53,39 @@ export default function FormInput({
             </p>
           }
         </div>
-        <div className="col-span-4">
-          {
-            props.type === 'checkbox' ?
-              <label
-                htmlFor={name}
-                className={`relative inline-flex items-center font-medium text-xs ${errors[name] ? 'text-red-500' : ''} cursor-pointer`}>
-                <Input
-                  id={name}
-                  {...register(name)}
-                  {...props}
-                  className={`${errors[name] ? 'border-red-400' : ''} sr-only peer ${label ? 'mr-1' : ''}`}
-                />
-
-                <div className="w-11 h-6 bg-gray-200 rounded-full peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
-
-              </label>
-              :
+      )}
+      <div className="col-span-4">
+        {
+          props.type === 'checkbox' ?
+            <label
+              htmlFor={name}
+              className={`relative inline-flex items-center font-medium text-xs ${errors[name] ? 'text-red-500' : ''} cursor-pointer`}>
               <Input
                 id={name}
                 {...register(name)}
                 {...props}
-                className={`${errors[name] ? 'border-red-400' : ''}`}
+                className={`${errors[name] ? 'border-red-400' : ''} sr-only peer ${label ? 'mr-1' : ''}`}
               />
-          }
-        </div>
 
+              <div className="w-11 h-6 bg-gray-200 rounded-full peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
+
+            </label>
+            :
+            <Input
+              id={name}
+              {...register(name)}
+              {...props}
+              className={`${errors[name] ? 'border-red-400' : ''}`}
+            />
+        }
+
+        {errors[name] && (
+          <Alert variant="danger" className="mt-2 text-xs">
+            {String(errors[name]?.message)}
+          </Alert>
+        )}
       </div>
-      {errors[name] && (
-        <Alert variant="danger" className="mt-2 text-xs">
-          {String(errors[name]?.message)}
-        </Alert>
-      )}
+
     </div>
   )
 }
