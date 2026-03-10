dd740763d052e8af83c6bd7ef0a7d837cf964230
iamelme <elme.delossantos@gmail.com>
2026-03-11 00:17:04 +0800

    refactor: move to main folder where it belongs

--- src/renderer/src/interfaces/ISettingRepository.ts
+++
@@ -1,8 +0,0 @@
-import { SettingsType } from "../features/settings/utils/type";
-
-export interface ISettingRepository {
-  get: () => { data: SettingsType | null; error: Error | string }
-  updateLocale: (locale: string) => { success: boolean; error: Error | string }
-  uploadLogo: (path: string) => { data: string; error: Error | string }
-  update: (params: Partial<SettingsType>) => { success: boolean, error: Error | string }
-}
