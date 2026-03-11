cfd3ec98cf10cb4cd69adb750ba894a6d581ac5a
iamelme <elme.delossantos@gmail.com>
2026-03-12 01:34:34 +0800

    refactor: settings type

--- src/renderer/src/features/settings/utils/type.ts
+++ src/renderer/src/features/settings/utils/type.ts
@@ -1,7 +1,18 @@
+// export type SettingsType = {
+//   locale: string;
+//   logo: string;
+//   tax: number;
+//   isTaxInclusive: boolean;
+//   isRedirectToSale: boolean;
+// };
+
 export type SettingsType = {
-  locale: string;
-  logo: string;
-  tax: number;
-  isTaxInclusive: boolean;
-  isRedirectToSale: boolean;
+  key: string;
+  value: string;
+};
+
+export type SettingsParamType = {
+  logo?: string;
+  tax?: number;
+  is_redirect_to_sales?: number;
 };
