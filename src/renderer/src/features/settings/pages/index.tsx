b79ae6e4da5a7d40c0418c09ca00036bf9a43af9
iamelme <elme.delossantos@gmail.com>
2026-03-12 17:22:56 +0800

    style: layout content scrollable

--- src/renderer/src/features/settings/pages/index.tsx
+++ src/renderer/src/features/settings/pages/index.tsx
@@ -18,15 +18,14 @@ const menu = [
 
 export default function SettingsPage(): ReactNode {
   return (
-    <div className="py-4">
-      <header className="mb-5">
+    <div className="flex flex-col h-[100svh] py-4">
+      <header className="shrink-0 mb-5">
         <h2 className="text-xl">Settings</h2>
         <p className="text-slate-400">Configure the app to your likes</p>
+        <SettingsNav items={menu} />
       </header>
 
-      <SettingsNav items={menu} />
-
-      <div className="flex-1 py-2">
+      <div className="flex-1 overflow-x-hidden overflow-y-auto py-2">
         <Outlet />
       </div>
     </div>
