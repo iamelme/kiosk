5f3876898f2c17bbede8c114b81a1a70166057ff
iamelme <elme.delossantos@gmail.com>
2026-03-12 23:56:30 +0800

    feat: add trigger to count deletion

--- src/main/migrations/00006_2026-03-11_create_trigger_for_categories_counter.sql
+++ src/main/migrations/00006_2026-03-11_create_trigger_for_categories_counter.sql
@@ -13,3 +13,17 @@ BEGIN
     count = count + 1;
 
 END;
+
+
+CREATE TRIGGER IF NOT EXISTS trg_after_delete_categories
+AFTER DELETE ON  categories
+BEGIN
+
+  UPDATE
+    counter
+  SET
+    count = count - 1
+  WHERE
+    name = 'categories';
+
+END;
